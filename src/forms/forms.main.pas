d5c4195de4a48e1a7e84643c80203c26244358f8
Gustavo Carreno <guscarreno@gmail.com>
2023-12-16 02:24:22 +0000

    feat: Renamed template GUI app, just skeleton

---
+++ src/forms/forms.main.pas
@@ -0,0 +1,26 @@
+unit Forms.Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmMain = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
